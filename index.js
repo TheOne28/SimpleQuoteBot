9c8cad2b8b43581d4e8b37c7abab288c39a8f803
Vincent Prasetiya A <72696277+TheOne28@users.noreply.github.com>
2021-12-16 15:34:13 +0000

    Basic bot client

--- index.js
+++ index.js
@@ -1,8 +1,9 @@
 const Discord = require("discord.js")
 const client = new Discord.Client()
+const token = process.env['TOKEN']
 
-client.on("ready", () =>{
-	console.log("Logging in as ${client.user.tag}")
+client.on("ready", () => {
+	console.log('Logging in as $ {client.user.tag}')
 })
 
-client.login()
+client.login(token)
